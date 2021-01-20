5f43662fc62d3245a3170249b0638fdf3ab28e98
mavrik83 <rjhuber83@gmail.com>
2021-01-20 10:04:39 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -6,11 +6,25 @@ class Timer extends Component {
     this.timer = React.createRef();
     this.state = {
       time: 0,
-      color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+      color: "#" + Math.floor(Math.random() * 16777215).toString(16),
     };
   }
 
   //Your code here
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate() {
+    this.timer.current.style.color =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+    this.timer.current.style.width = 240 + (this.state.time * 5) / 1000 + "px";
+    this.timer.current.style.height = 150 + (this.state.time * 5) / 1000 + "px";
+  }
 
   componentDidMount() {
     this.interval = setInterval(
@@ -24,7 +38,7 @@ class Timer extends Component {
   }
 
   render() {
-    const { time, color, logText } = this.state;
+    const { time, color, className, logText } = this.state;
     return (
       <section className="Timer" style={{ background: color }} ref={this.timer}>
         <h1>{time}</h1>
@@ -36,8 +50,8 @@ class Timer extends Component {
   }
 
   clockTick = () => {
-    this.setState(prevState => ({
-      time: prevState.time + this.props.updateInterval
+    this.setState((prevState) => ({
+      time: prevState.time + this.props.updateInterval,
     }));
   };
 
